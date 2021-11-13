3cde3c07ca6c6f313a1c62f6d4d2f49a35fde89e
Javier Levio <javier.levio.12@sansano.usm.cl>
2021-11-13 20:06:14 -0300

    Fixed initial definition

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { Client, Message } from 'discord.js';
 
 const client = Client()
 
-export default () => {
-    client.on('ready', () => {console.log(`Logged in as ${client.user.tag}`)})
-    client.on('message', (message: Message) => {if (message.content === 'ping') {message.reply('pong')}})
-    client.login(process.env.TOKEN)
-}
+client.on('ready', () => {console.log(`Logged in as ${client.user.tag}`)})
+client.on('message', (message: Message) => {if (message.content === 'ping') {message.reply('pong')}})
+client.login(process.env.TOKEN)
